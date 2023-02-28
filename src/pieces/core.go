77b709d307b3828b118513aae710248445a5662b
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2023-02-28 09:35:22 -0600

    Update core.go

--- src/pieces/core.go
+++ src/pieces/core.go
@@ -1,12 +1,12 @@
 package pieces
 
 const (
-	KING   = 0
-	QUEEN  = 1
-	ROOK   = 2
-	BISHOP = 3
-	KNIGHT = 4
-	PAWN   = 5
+	KING   = iota
+	QUEEN  = iota
+	ROOK   = iota
+	BISHOP = iota
+	KNIGHT = iota
+	PAWN   = iota
 )
 
 type Piece struct {
